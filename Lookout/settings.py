be047137ad376d2c3f5e1ca06f2e534aad8dd0c3
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-06-25 16:53:54 -0700

    Added AQI API

--- Lookout/settings.py
+++ Lookout/settings.py
@@ -5,10 +5,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
@@ -16,7 +12,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -65,7 +60,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Lookout.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -75,7 +69,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -95,6 +88,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# API Keys
+AIRQUALITY_API_KEY = config('AIRQUALITY_API_KEY')
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -108,7 +103,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- lookout_app/views.py
+++ lookout_app/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from django.conf import settings
+import requests
+import json
 
 def home(request):
+
+	key = settings.AIRQUALITY_API_KEY
+
+	url = "http://api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key=" + key
+
+	payload={}
+	headers = {}
+
+	response = requests.request("GET", url, headers=headers, data=payload)
+	aqi_data = json.loads(response.text)
+
 	context = {
-		'title': 'Home'
+		'title': 'Home',
+		'aqi_index': aqi_data['status']
 	}
 	return render(request, 'lookout_app/home.html', context)
